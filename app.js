adccfbc0ef34f03079d2bc155ccbda4aaebb2535
naomi-oleary <oleary.ng@gmail.com>
2026-01-31 09:49:47 -0700

    next: make it my own

--- app.js
+++ app.js
@@ -2,6 +2,32 @@
 // service_8mw3u2p
 // gb1WGF_NbvIc6wReE
 
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1/20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !==0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact() {
     event.preventDefault()
     const loading = document.querySelector('.modal__overlay--loading')
@@ -23,4 +49,13 @@ function contact() {
                 "The email service is temporarily unavailable. Please contact me directly at oleary.ng@gmail.com"
             )
         })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
 }
